4271fabccec83d35a962d42d577b64d7ee1e6227
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-09-13 10:07:14 +0200

    Interface

---
+++ Server/RepositoryContracts/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entitities;
+
+namespace RepositoryContracts;
+
+public interface IUserRepository
+{
+    Task<User> AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(int id);
+    Task<User> GetSingleAsync(int id);
+    IQueryable<User> GetMany();
+}
