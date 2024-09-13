aaf99e651acb71184be020bd7c277a267c69c09b
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-09-13 09:55:36 +0200

    Post

---
+++ Server/Entitities/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace Entitities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string body { get; set; }
+    public int UserId { get; set; }
+}
