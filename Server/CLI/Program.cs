970b97a63165f867f329642ccc11dd45cef50fb1
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-09-17 17:53:49 +0200

    Part 2 start

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CLI;
+using InMemoryRepositories;
+using RepositoryContracts;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting CLI Appp...");
+        IUserRepository userRepository = new UserInMemoryRepository();
+        ICommentRepository commentRepository = new CommentInMemoryRepository();
+        IPostRepository postRepository = new PostInMemoryRepository();
+        
+        
+    }
+}
