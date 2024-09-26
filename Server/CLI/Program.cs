c7352becfbeb68a4504eae374f2dcc08aa335b20
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-09-26 17:18:46 +0200

    User added + post

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,17 +1,17 @@
 ﻿using CLI.UI;
 using CLI.UI.ManageUsers;
+using FileRepositories;
 
 namespace CLI;
-using InMemoryRepositories;
 using RepositoryContracts;
 class Program
 {
     static async Task Main(string[] args)
     {
         Console.WriteLine("Starting CLI Appp...");
-        IUserRepository userRepository = new UserInMemoryRepository();
-        ICommentRepository commentRepository = new CommentInMemoryRepository();
-        IPostRepository postRepository = new PostInMemoryRepository();
+        IUserRepository userRepository = new UserFileRepository();                          // Old: UserInMemoryRepository();
+        ICommentRepository commentRepository = new CommentFileRepository();                        // Old: CommentInMemoryRepository();
+        IPostRepository postRepository = new PostFileRepository();                        // Old: PostInMemoryRepository();
 
         CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
         await cliApp.StartAsync();
